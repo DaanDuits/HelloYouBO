ab3ae4aaa324b827c4def31cb28ce3889805b14c
DaanDuits <daand04@gmail.com>
2021-11-04 10:32:47 +0100

    Eight commit

--- Beroepsopdracht.py
+++ Beroepsopdracht.py
@@ -15,11 +15,42 @@ Tekst7 = "Bij de bergen aangekomen moeten je beslissen of je om de bergen heen g
 Tekst8 = "Bij de bergen aangekomen moet je kiezen of je er omheen gaat lopen of over de bergen heen gaat."
 Tekst9 = "Je besluit met de groep mee te gaan en je volgt ze richting de bergen."
 Tekst9n2 = "Bij de bergen aangekomen moeten jullie als groep kiezen of jullie over de bergen gaan of om de bergen heen jullie keizen om over de bergen te gaan."
-Tekst10 = "In de bergen worden jullie staande gehouden door een stel mensen van de taliban ze herkennen sommige van jullie van de dreigbriefen en jullie worden opgepakt en meegenomen."
+Tekst10 = "Jullie besluiten om de bergen te reiden, na een lange reis van een ongeveer dag komen jullie aan bij de grens."
+Tekst11 = "Jullie besluiten om over de bergen te lopen na een lange reis van ongeveer een paar dagen komen jullie bij de grens aan"
+Tekst12 = "Jullie besluiten om om de bergen heen te lopen na een lange reis van ongeveer een paar dagen komen jullie bij de grens aan"
+Tekst13 = "In de bergen worden jullie staande gehouden door een stel soldaten van de taliban ze herkennen sommige van jullie van de dreigbriefen en jullie worden opgepakt en meegenomen."
+Tekst14 = "Bij de grens zien jullie 2 mensen bij een auto staan, mischien kun je met hun mee? Maar jullie kunnen ook zelf de grens over vluchten op de voet."
+Tekst15 = "Jullie besluiten door te reiden over de grens en richting europa te reiden. Maar welk land?"
+Tekst16 = "Jullie besluiten de grens over telopen totdat jullie geschreeuw achter jullie horen het zij een paar soldaten van de taliban en ze weten precies wat jullie proberen."
+Tekst17 = "Jullie lopen richting de mensen en begint met hun te praten ze helpen mensen de grens over. Je voeld je blij je ben gevlugd maar waar ga je nu naartoe?"
+Tekst18 = "In Nederland aangekomen worden jullie naar een vluchtelingen asiel gestuurd na een paar maanden zijn jullie vrij in Nederland."
+Tekst19 = "In Griekeland aangekomen worden jullie naar een vluchtelingen asiel gestuurd maar naar een paar weken worden jullie terug gestuurd."
+Tekst20 = "In de toekomst wil jij ook jouw vrouw en andere kinderen naar Nederkland krijgen maar dat gaat nog wel een paar jaar duren."
+
 end =0
 vn = 0
+v = 0
 while end == 1:
-    print("Je ben opgepakt en meegenomen door de taliban jouw en jouw kinds lot worden in hun handen gelegd.")
+    print("Je ben opgepakt en meegenomen door de taliban jouw en jouw kind's lot worden in hun handen gelegd.")
+    v = 1
+while end == 2:
+    print("Bij de grens wordt je neergeschoten door de soldaten van de taliban en jouw kind word meegenomen.")
+    v = 1
+while end == 3:
+    print("Je bent terug gestuurd naar Afghanistan waar je nu in de gaten word gehouden door de taliban")
+    v = 1
+while end == 4:
+    print("Je bent gevlucht en vrij in Nederland wat je wilt wat er in de toekomst gaat gebeuren gaat nog even duren maar het komt vas goed.")
+    v = 1
+while v == 1:
+    print("Wil je mijn verhaal nog een keer volgen? Y/N")
+    ant = input()
+    if ant == "Y":
+        end =0
+        vn = 0
+        v = 0
+    if ant == "N":
+        break
 while vn == 0:
     print(Intro)
     print(Intro2)
@@ -93,16 +124,59 @@ while vn == 8:
     if ant == "A":
         vn = 12
     elif ant == "B":
-        vn = 13
+        vn = 11
 while vn == 9:
     print(Tekst9)
     print(Tekst9n2)
-    print("B. Over de bergen lopen")
-    ant = input()
-    if ant == "A":
-        vn = 14
-    elif ant == "B":
-        vn = 15
+    vn = 13
 while vn == 10:
     print(Tekst10)
+    vn = 15
+while vn == 11:
+    print(Tekst11)
+    vn = 14
+while vn == 12:
+    print(Tekst12)
+    vn = 14
+while vn == 13:
+    print(Tekst13)
     end = 1
+while vn == 14:
+    print(Tekst14)
+    print("A. Op de voet de grens over")
+    print("B. Naar de mensen met de auto toe gaan")
+    ant = input()
+    if ant == "A":
+        vn = 16
+    elif ant == "B":
+        vn = 17
+while vn == 15:
+    print(Tekst15)
+    print("A. Nederland")
+    print("B. Griekeland")
+    ant = input()
+    if ant == "A":
+        vn = 18
+    elif ant == "B":
+        vn = 19
+while vn == 16:
+    print(Tekst16)
+    end = 2
+while vn == 17:
+    print(Tekst17)
+    print("A. Nederland")
+    print("B. Griekeland")
+    ant = input()
+    if ant == "A":
+        vn = 18
+    elif ant == "B":
+        vn = 19
+while vn == 18:
+    print(Tekst18)
+    vn = 20
+while vn == 19:
+    print(Tekst19)
+    end = 3
+while vn == 20:
+    print(Tekst20)
+    end = 4
